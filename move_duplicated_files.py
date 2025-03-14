0c53e244fa282a3ae47378f492c16fd0536d93f6
Daniel.Correa.Srcontabil <daniel.correa@senhorcontabil.com.br>
2025-03-14 08:49:36 -0300

    Altera o arquivo para classe.

--- move_duplicated_files.py
+++ move_duplicated_files.py
@@ -1,35 +1,46 @@
 import os
 import shutil
 import re
+from file_lister import FileLister
 
-def listar_pdfs(diretorio):
-    return [f for f in os.listdir(diretorio) if f.endswith(".pdf")]
+class DuplicatedFilesMover:
+    def __init__(self, directory):
+        self.directory = directory
+        self.duplicates_folder = os.path.join(directory, "duplicates")
+        os.makedirs(self.duplicates_folder, exist_ok=True)
+        self.seen = {}
 
-def extrair_chave(nome_arquivo):
-    """
-    Extrai a chave única (CNPJ + EmpresaID) do nome do arquivo.
-    """
-    padrao = r"(\d{14})"
-    match = re.match(padrao, nome_arquivo)
-    if match:
-        return match.group(1)  # Retorna CNPJ e EmpresaID
-    return None
+    def listar_pdfs(self, directory):
+        return [f for f in os.listdir(directory) if f.endswith(".pdf")]
 
-def mover_duplicados(diretorio):
-    arquivos = listar_pdfs(diretorio)
-    vistos = {}
-    pasta_duplicados = os.path.join(diretorio, "duplicado")
-    os.makedirs(pasta_duplicados, exist_ok=True)
-    
-    for arquivo in arquivos:
-        chave = extrair_chave(arquivo)
-        if chave:
-            if chave in vistos:
-                shutil.move(os.path.join(diretorio, arquivo), os.path.join(pasta_duplicados, arquivo))
-                print(f"Movendo duplicado: {arquivo}")
-            else:
-                vistos[chave] = arquivo
+    def extrair_chave(self, nome_arquivo):
+        """
+        Extrai a chave única (CNPJ + EmpresaID) do nome do arquivo.
+        """
+        padrao = r"(\d{14})"
+        match = re.match(padrao, nome_arquivo)
+        if match:
+            return match.group(1)  # Retorna CNPJ e EmpresaID
+        return None
+
+    def mover_duplicados(self):
+        lister = FileLister(self.directory, ".pdf")
+        files = lister.list_files()
+        vistos = {}
+        pasta_duplicados = os.path.join(self.directory, "duplicado")
+        os.makedirs(pasta_duplicados, exist_ok=True)
+        
+        for file in files:
+            chave = self.extrair_chave(file)
+            if chave:
+                if chave in vistos:
+                    shutil.move(os.path.join(self.directory, file), os.path.join(pasta_duplicados, file))
+                    print(f"Movendo duplicado: {file}")
+                else:
+                    vistos[chave] = file
     
 if __name__ == "__main__":
-    diretorio_alvo = "C:\\Users\\Daniel_Maciel\\Desktop\\Nomear\\Declaracao"
-    mover_duplicados(diretorio_alvo)
+    directory = input("Pasta onde estão os arquivos:")
+    mover = DuplicatedFilesMover(directory)
+    mover.mover_duplicados()
+    
